36a6445a1840a9aad9c56c26409fc07b1a2241cf
Little-Sparroh <wingedoracle@tutanota.com>
2025-09-17 12:12:49 +0000

    Add files via upload

---
+++ obj/Release/net48/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace DisableFloatingText
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "DisableFloatingText";
+        public const string PLUGIN_NAME = "DisableFloatingText";
+        public const string PLUGIN_VERSION = "..";
+    }
+}
