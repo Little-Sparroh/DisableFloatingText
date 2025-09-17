8f1a3c350466b23b4b047c205d6ec242c976ee0c
Little-Sparroh <wingedoracle@tutanota.com>
2025-09-17 19:43:27 +0000

    Delete obj/Release/net48 directory

--- obj/Release/net48/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace DisableFloatingText
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "DisableFloatingText";
-        public const string PLUGIN_NAME = "DisableFloatingText";
-        public const string PLUGIN_VERSION = "..";
-    }
-}
